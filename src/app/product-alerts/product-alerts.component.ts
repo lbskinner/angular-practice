cdb2d84f2b4a2489064104a5680ca8e810f95a2a
Leila Skinner <beileiwang@gmail.com>
2020-08-06 11:51:42 -0500

    using output and eventemitter to create notify event and bind event to parent

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,7 +10,9 @@ import { Input } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
   // @Input() decorator indicates the property value passes in from the component's parent, the product list component
   @Input() product;
-
+  // define a property with an @Output() decorator and an instance of EventEmitter()
+  // this allows the product-alert component to emit an event when the value of the notify property changes
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
