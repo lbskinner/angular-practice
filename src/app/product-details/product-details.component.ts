bda4e19a72c9aeb81904168d1e8b0c4b0dafc697
Leila Skinner <beileiwang@gmail.com>
2020-08-10 15:35:47 -0500

    use cart service to add items to cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,10 +13,18 @@ export class ProductDetailsComponent implements OnInit {
   // define the product property
   product;
 
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
   // The ActivatedRoute is specific to each routed component that the Angular Router loads.
   // It contains information about the route, its parameters, and additional data associated with the route.
   // inject the ActivatedRoute into the constructor by adding it as an argument within the constructor's parentheses
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   //subscribe to route parameters and fetch the product based on the productId
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
