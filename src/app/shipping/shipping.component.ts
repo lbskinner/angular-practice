fa6611865a1baaa228c435716530dc85420c9e6a
Leila Skinner <beileiwang@gmail.com>
2020-08-10 16:05:14 -0500

    added shipping costs link to cart view, added shipping costs view and display data

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
 })
 export class ShippingComponent implements OnInit {
+  // Define a shippingCosts property
+  shippingCosts;
 
-  constructor() { }
+  // Inject the cart service in the ShippingComponent constructor
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    // Set the shippingCosts property using the getShippingPrices() method from the cart service
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
-
 }
