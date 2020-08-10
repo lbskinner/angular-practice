24be365e40ff5eca2e9e249e59898dc60723426c
Leila Skinner <beileiwang@gmail.com>
2020-08-10 15:47:13 -0500

    using cart service to display items in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  // Define the items property to store the products in the cart
+  items;
 
-  constructor() { }
+  // Inject the CartService so that the cart component can use it
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    // Set the items using the cart service's getItems() method from CartService
+    this.items = this.cartService.getItems();
   }
-
 }
